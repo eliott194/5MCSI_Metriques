a53aaacbda6e117de439ea5719854cf630535c6c
eliott194 <55228898+eliott194@users.noreply.github.com>
2024-02-12 15:59:48 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -20,6 +20,16 @@ def mongraphique():
 def monhistogramme():
     return render_template("histogramme.html")
 
+@app.route("/commits/")
+def moncommits():
+    return render_template("commits.html")
+
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.minute
+        return jsonify({'minutes': minutes})
+
 @app.route('/paris/')
 def meteo():
     response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
